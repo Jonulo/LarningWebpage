c475e0169eff8048267c5f820b640134244f0aa7
Jorge Nunez <georgenul@live.com>
2021-06-05 16:33:38 -0500

    responsive functionality for sidebar added

--- src/utils/sideBarMenu/index.js
+++ src/utils/sideBarMenu/index.js
@@ -5,6 +5,7 @@ function openCloseSideBar() {
   let openNavButton = document.getElementById("sideBar-btn");
   let menuIsOpen = true;
 
+
   sideBarFuntionality(menuIsOpen)
   openNavButton.removeEventListener("click", () => {
     menuIsOpen = !menuIsOpen;
@@ -19,17 +20,35 @@ function openCloseSideBar() {
 }
 
 function sideBarFuntionality(menuIsOpen) {
+  let currentScreenWidth = screen.width;
+  let sideBarWidth = "40%";
+  let contentWidth = "60%";
+  let closeSideBarWidth = "8%";
+  let sideBar = document.getElementById("sidebar");
+  let contentTag = document.getElementById("content");
+
+  if(currentScreenWidth >= 767 && currentScreenWidth < 1200) {
+    sideBarWidth = "30%";
+    contentWidth = "70%";
+    closeSideBarWidth = "5%";
+  }else if(currentScreenWidth >= 1200) {
+    sideBarWidth = "20%";
+    contentWidth = "80%";
+  }
+
   if(menuIsOpen) {
-    document.getElementById("content").style.marginLeft = "20%";
-    document.getElementById("content").style.width= "80%";
-    document.getElementById("sidebar").style.width = "20%";
+    contentTag.style.marginLeft = sideBarWidth;
+    contentTag.style.width= contentWidth;
+    sideBar.style.width = sideBarWidth;
+    sideBar.classList.add('sidebar-open');
     document.getElementById("sideBar-btn").textContent = "CLOSE";
     document.getElementById("sidebar__menu").style.display = "block";
   } else if(!menuIsOpen) {
-    document.getElementById("content").style.marginLeft = "5%";
-    document.getElementById("content").style.width= "95%";
-    document.getElementById("sidebar").style.width = "5%";
-    document.getElementById("sideBar-btn").textContent = "OPEN";
+    contentTag.style.marginLeft = closeSideBarWidth;
+    contentTag.style.width= "95%";
+    sideBar.style.width = closeSideBarWidth;
+    sideBar.classList.remove('sidebar-open');
+    document.getElementById("sideBar-btn").textContent = ">";
     document.getElementById("sidebar__menu").style.display = "none";
   }
 
